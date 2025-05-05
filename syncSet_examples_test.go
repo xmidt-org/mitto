86e1a12b7cc7a281231bedfa35cce9b5571fd845
johnabass <johnbass1017@yahoo.com>
2025-05-04 20:31:15 -0700

    chore: moved this example to a more appropriate file

--- syncSet_examples_test.go
+++ syncSet_examples_test.go
@@ -5,15 +5,15 @@ package mitto
 
 import "fmt"
 
-func ExampleSet_Add() {
-	var s Set[int] // int is just an example, this could be a struct
-	s.Add(
+func ExampleSyncSet_Add() {
+	var ss SyncSet[int] // int is just an example, this could be a struct
+	ss.Add(
 		AsListener[int](func(event int) {
 			fmt.Println(event)
 		}),
 	)
 
-	s.Send(999)
+	ss.Send(999)
 
 	// Output:
 	// 999
